3224125b25a062388fcc2a6f625e513cca94d625
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-05 10:21:49 +0530

    update following code lines

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-    <form action="/action_page.php">
+    <form action="action.php" method="POST">
         
     <div class="container">
             
@@ -23,33 +23,53 @@
         <hr>
 
         <label for="first_name"><b>First Name</b></label>
-        <input type="text" placeholder="First Name Here..." name="fname" id="fname" required>
+        <input type="text" placeholder="First Name Here..." name="fname" id="fname">
 
         <label for="last_name"><b>Last Name</b></label>
-        <input type="text" placeholder="Last Name Here..." name="lname" id="lname" required>
+        <input type="text" placeholder="Last Name Here..." name="lname" id="lname" >
     
         <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" id="email" required>
+        <input type="text" placeholder="Enter Email..." name="email" id="email" >
 
         <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+        <input type="password" placeholder="Enter Password..." name="psw" id="psw" >
 
         <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+        <input type="password" placeholder="Repeat Password..." name="psw-repeat" id="psw-repeat" >
     
         <hr>
     
         <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
     
-        <button type="submit" class="registerbtn">Register</button>
+        <button type="submit" name="submit" class="registerbtn">Register</button>
   
     </div>
-  
+
+    <?php if(isset($_GET['error_message'])){ ?>
+    
+    <?php $message = $_GET['error_message'];
+    
+    echo"<p style='color: firebrick'>$message</p>"
+    
+    ?>
+    
+    <?php }?>
+
+    <?php if(isset($_GET['message'])){ ?>
+
+    <?php
+    
+        $message = $_GET['message'];
+
+        echo"<p style='color: darkgreen'>$message</p>"?>
+
+    <?php }?>
   
     <div class="container signin">
-        <p>Already have an account? <a href="#">Sign in</a>.</p>
+        <p>Already have an account? <a href="signin.php">Sign in</a>.</p>
     </div>
+
 </form>
 
 </body>
