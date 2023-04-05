21b741291a18f82369857f1b629789ea066eab2b
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-05 10:11:47 +0530

    design frontend of the home.php file

--- home.php
+++ home.php
@@ -7,17 +7,38 @@ if(!isset($_SESSION['id']))
     header("location: sigin.php");
 }
 
-echo $_SESSION['mail'];
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <link rel="stylesheet" href="profile-card.css">
+
+        <title>Profile Card</title>
+    </head>
+<body>
+
+<div class="card">
+  
+  <h1><?php echo 'Hello '. $_SESSION['last_name']?></h1>
+  
+  <p class="title"><?php echo $_SESSION['first_name']?><?php echo' '.$_SESSION['last_name']?></p>
+  
+  <p><?php echo $_SESSION['mail']?></p>
+  <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+  </div>
+  
+  <p><button>Contact</button></p>
+
+</div>
+
+</body>
+
+</html>
